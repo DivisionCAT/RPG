a61c904fe4c817c88832065d736557c28fb3d559
An Nyeong <me@annyeong.me>
2019-10-24 17:16:54 +0900

    Updates

---
+++ precache-manifest.0f9151e412aa1c3a24f47c55716ee997.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a032b83a01d4744538eed81cdf96dd7",
+    "url": "/RPG/index.html"
+  },
+  {
+    "revision": "b6c066d8c3cb675693f5",
+    "url": "/RPG/static/css/main.47cb7fcd.chunk.css"
+  },
+  {
+    "revision": "115e7ab429f9a2ab8640",
+    "url": "/RPG/static/js/2.20e97030.chunk.js"
+  },
+  {
+    "revision": "b6c066d8c3cb675693f5",
+    "url": "/RPG/static/js/main.98efebb5.chunk.js"
+  },
+  {
+    "revision": "ae17d79ed7155d0436c9",
+    "url": "/RPG/static/js/runtime-main.cd2b49bc.js"
+  }
+]);
